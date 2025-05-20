f76cce122aa037cd4ad5ddf1d5b847e03ebcb938
Júlio Andherson <julioandherson1@gmail.com>
2025-05-19 23:05:06 -0300

    first commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Júlio Andherson de Oliveira Silva on 19/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
