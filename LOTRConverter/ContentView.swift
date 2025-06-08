c99392db4bdd9eb35105fc738c6eb464ead089a9
Júlio Andherson <julioandherson1@gmail.com>
2025-06-08 12:36:16 -0300

    creating conversion and focused attribute to textfield

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
     @State var leftAmount = ""
     @State var rightAmount = ""
     
+    @FocusState var leftTyping
+    @FocusState var rightTyping
+    
     @State var leftCurrency: Currency = .silverPiece
     @State var rightCurrency: Currency = .goldPiece
     
@@ -62,9 +65,7 @@ struct ContentView: View {
                         // Convertion Input
                         TextField("Amount", text: $leftAmount)
                             .textFieldStyle(.roundedBorder)
-                            
-                        
-                    
+                            .focused($leftTyping)
                     }
                     
                     // =
@@ -72,9 +73,7 @@ struct ContentView: View {
                         .font(.largeTitle)
                         .foregroundStyle(.white)
                         .symbolEffect(.pulse)
-                    
-                    
-                    
+  
                     // Right Side
                     VStack {
                         
@@ -100,12 +99,14 @@ struct ContentView: View {
                         TextField("Amount", text: $rightAmount)
                             .textFieldStyle(.roundedBorder)
                             .multilineTextAlignment(.trailing)
+                            .focused($rightTyping)
                     }
                     
                 }
                 .padding()
                 .background(.black.opacity(0.5))
                 .clipShape(.capsule)
+                .keyboardType(.decimalPad)
                 
                 Spacer()
                 
@@ -122,12 +123,29 @@ struct ContentView: View {
                             .foregroundStyle(.white)
                     }
                     .padding(.trailing)
+                    .onChange(of: leftAmount) {
+                        if leftTyping {
+                            rightAmount = leftCurrency.convert( leftAmount, to: rightCurrency)
+                        }
+                    }
+                    .onChange(of: rightAmount) {
+                        if rightTyping {
+                            leftAmount =  rightCurrency.convert(rightAmount, to: leftCurrency)
+                        }
+                    }
+                    .onChange(of: leftCurrency) {
+                        rightAmount = leftCurrency.convert( leftAmount, to: rightCurrency)
+                    }
+                    .onChange(of: rightCurrency) {
+                        leftAmount =  rightCurrency.convert(rightAmount, to: leftCurrency)
+                    }
                     .sheet(isPresented: $showExchangeInfo) {
                         ExchangeInfo()
                     }
                     .sheet(isPresented: $showSelectCurrency) {
                         SelectCurrency(topCurrency: self.$leftCurrency, bottomCurrency: self.$rightCurrency)
                     }
+
                 }
             }
         }
--- LOTRConverter/Currency.swift
+++ LOTRConverter/Currency.swift
@@ -46,4 +46,12 @@ enum Currency: Double, CaseIterable, Identifiable {
             "Gold Piece"
         }
     }
+    
+    func convert(_ amountString: String, to currency: Currency) -> String {
+        guard let doubleAmount: Double = Double(amountString) else { return "" }
+        
+        let convertedAmount = (doubleAmount / self.rawValue) * currency.rawValue
+        
+        return String(format: "%.2f", convertedAmount)
+    }
 }
